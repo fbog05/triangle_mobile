e6b9be97331991434db91b0065e93f95f48003c4
Ferencsik Boglárka <boglarkaferencsik05@gmail.com>
2024-02-14 08:55:54 +0100

    0214855

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [base, setBase] = useState('')
+  const [height, setHeight] = useState('')
+  const [area, setArea] = useState('')
+  
+  function calcArea(){
+    let res = base * height /2
+    setArea(res)
+    setBase('')
+    setHeight('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.cim}>Háromszög</Text>
+
+      <Text style={styles.szoveg}>Alap</Text>
+      <TextInput 
+        style={styles.mezo}
+        onChangeText={text => setBase(text)}
+      />
+
+      <Text style={styles.szoveg}>Magasság</Text>
+      <TextInput 
+        style={styles.mezo}
+        onChangeText={text => setHeight(text)}
+      />
+
+      <Pressable
+        onPress={calcArea}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>
+          SZÁMÍT
+        </Text>
+      </Pressable>
+
+      <Text style={styles.szoveg}>Terület</Text>
+      <TextInput 
+        style={styles.mezo}
+        value = {area}
+      />
+
     </View>
   );
 }
@@ -13,8 +53,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  cim: {
+    color: 'navy',
+    fontWeight: 800,
+    fontSize: 25,
+    paddingBottom: 25,
+  },
+
+  szoveg:{
+    color: 'navy',
+    fontWeight: 700,
+    paddingBottom: 10,
+    fontSize: 18,
+  },
+
+  mezo: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 8,
+    color: 'navy',
+    fontWeight: 600,
+    marginBottom: 20,
+  },
+
+  button: {
+    backgroundColor: 'navy',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginBottom: 25,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 700,
   },
 });
